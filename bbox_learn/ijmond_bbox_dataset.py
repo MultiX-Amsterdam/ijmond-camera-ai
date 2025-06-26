c0d69130bb8e7ae2de6d6ef237affbd1039f4d05
Yen-Chia Hsu <hsu.yenchia@gmail.com>
2025-06-26 12:49:49 +0200

    Add resize transformation to the test case of bounding box.

--- bbox_learn/ijmond_bbox_dataset.py
+++ bbox_learn/ijmond_bbox_dataset.py
@@ -70,6 +70,7 @@ if __name__ == "__main__":
         v2.RandomHorizontalFlip(p=0.5),
         v2.ToDtype(torch.float32, scale=True),
         v2.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+        v2.Resize((400, 400), antialias=True)
     ])
 
     DT = IjmondBboxDataset(metadata_path=metadata_path, root_dir=root_dir, transform=transforms)
