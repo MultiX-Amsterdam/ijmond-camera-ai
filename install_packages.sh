41eba1ffcb38e8fb834cc1d015b6b4bc2f8feac6
Yen-Chia Hsu <hsu.yenchia@gmail.com>
2025-10-01 19:22:48 +0000

    Edit the installation script.

--- install_packages.sh
+++ install_packages.sh
@@ -6,5 +6,5 @@ pip install --upgrade requests==2.32.4
 pip install --upgrade pillow==11.2.1
 pip install --upgrade matplotlib==3.10.3
 pip install --upgrade tqdm==4.67.1
-pip install git+https://github.com/pytorch-labs/segment-anything-fast.git
+pip install git+https://github.com/facebookresearch/segment-anything.git
 wget -P bbox_learn https://dl.fbaipublicfiles.com/segment_anything/sam_vit_l_0b3195.pth
